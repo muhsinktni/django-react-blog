fe398a55ff2bc54a44baf931567b87f86cad3cb5
Muhsin <mkmuhsin786@gmail.com>
2026-03-14 12:30:06 +0530

    Fix blog app import path

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'mysite.blog',
     'blog.apps.BlogConfig',
     'rest_framework',
     'rest_framework.authtoken',
